ecbd096e6c29929259e73f25350ec1c0f6309a17
jsta <stachel2@msu.edu>
2017-12-08 16:45:59 -0500

    make code runnable

--- scripts/pilot-dilution-profiles.R
+++ scripts/pilot-dilution-profiles.R
@@ -15,18 +15,22 @@
 #' pilot_dilution_profiles(fpath, outpath)
 
 pilot_dilution_profiles <- function(fpath, outpath, dim = NA){
-	#format incoming data
+	
+	# format incoming data ####
 	dt <- readxl::read_excel(fpath)
 	names(dt) <- make.names(names(dt)) %>% gsub("\\.", "", .) %>% gsub(" ", "", .)
-	dt[4:ncol(dt)] <- suppressWarnings(apply(dt[4:ncol(dt)], 2, function(x) as.numeric(x)))
+	dt[4:ncol(dt)] <- suppressWarnings(apply(dt[4:ncol(dt)], 2, 
+																					 function(x) as.numeric(x)))
 	dt$chamber <- as.factor(dt$chamber)
-	dt <- dt[nchar(dt$quad) == 1,] #remove surface water samples
+	dt <- dt[nchar(dt$quad) == 1,] # remove surface water samples
 	dt$days <- as.numeric((dt$date - min(dt$date)) / (60 * 60 * 24))
 	
-	#compute with-in chamber means
+	# compute with-in chamber means ####
 	dt <- dplyr::group_by(dt, days, chamber)
-	dt <- dplyr::summarise(dt, mean_sal = mean(sal, na.rm = TRUE), sd_sal = sd(sal, na.rm = TRUE), n = length(sal))
-	dt$se <- 2 * (dt$sd / sqrt(dt$n))
+	dt <- dplyr::summarise(dt, mean_sal = mean(sal, na.rm = TRUE), 
+												 sd_sal = sd(sal, na.rm = TRUE), n = length(sal))
+	
+	dt$se <- 2 * (dt$sd_sal / sqrt(dt$n))
 	dt$upper <- dt$mean_sal + dt$se
 	dt$lower <- dt$mean_sal - dt$se
 	dt$lower[dt$lower < 0] <- 0
@@ -51,16 +55,20 @@ pilot_dilution_profiles <- function(fpath, outpath, dim = NA){
 		legend.position = "none"))
 	
 	gg <- ggplot2::ggplot(data = dt, ggplot2::aes(x = days, y = mean_sal))
-	gg <- gg + ggplot2::geom_point(size = 3) + ggplot2::geom_errorbar(ggplot2::aes(ymin = lower, ymax = upper), width = 0.3, color = "black")
-	gg <- gg + ggplot2::geom_smooth(se = FALSE, color = "black")
-	gg + ggplot2::ylab("Salinity") +	ggplot2::xlab("Days") + theme_opts
+	gg <- gg + ggplot2::geom_smooth(se = FALSE, color = "gray")
+	gg <- gg + ggplot2::geom_point(size = 3) + 
+		ggplot2::geom_errorbar(ggplot2::aes(ymin = lower, ymax = upper), 
+													 width = 0.3, color = "black")
+	
+	gg + ggplot2::ylab("salinity") +	ggplot2::xlab("days post dosing") + 
+		theme_opts
 	
 	ggplot2::ggsave(filename = outpath, width = 7, height = 5)
 }
 
 library(magrittr)
-fpath <- list.files("../data", "*.xlsx", full.names = TRUE, include.dirs = TRUE)
-outpath <- file.path("../figures", "dilution-profile.png")
+fpath <- list.files("data", "*.xlsx", full.names = TRUE, include.dirs = TRUE)
+outpath <- file.path("figures", "dilution-profile.png")
 #fpath <- list.files("data", "*.xlsx", full.names = TRUE, include.dirs = TRUE) 
 #outpath <- file.path("figures", "dilution-profile.png")
 pilot_dilution_profiles(fpath, outpath)
